57c55f810321ed92df20ebbf0ff4747fa0fae7c6
sajicode <sajioloye@gmail.com>
2018-12-15 14:12:02 +0100

    First commit - Basic App Setup

---
+++ schema.js
@@ -0,0 +1,74 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = require('graphql');
+
+// Hardcoded data
+const customers = [{
+    id: '1',
+    name: 'Thierry Henry',
+    email: 'thierry@arsenal.dev',
+    age: 35
+  },
+  {
+    id: '2',
+    name: 'Dennis Bergkamp',
+    email: 'dennis@arsenal.dev',
+    age: 42
+  },
+  {
+    id: '3',
+    name: 'Robert Pires',
+    email: 'robert@arsenal.dev',
+    age: 37
+  }
+]
+
+// Customer type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: {
+      type: GraphQLString
+    },
+    name: {
+      type: GraphQLString
+    },
+    email: {
+      type: GraphQLString
+    },
+    age: {
+      age: GraphQLInt
+    }
+  })
+});
+
+// root query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: {
+          type: GraphQLString
+        }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id === args.id) {
+            return customers[i];
+          }
+        }
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
