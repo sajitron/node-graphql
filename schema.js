6be17696ca51ca35950fcf8bc8a87235d90daf59
sajicode <sajioloye@gmail.com>
2018-12-15 22:15:14 +0100

    add edit and delete mutations

--- schema.js
+++ schema.js
@@ -96,6 +96,37 @@ const mutation = new GraphQLObjectType({
 					})
 					.then((res) => res.data);
 			}
+		},
+		deleteCustomer: {
+			type: CustomerType,
+			args: {
+				id: {
+					type: new GraphQLNonNull(GraphQLString)
+				}
+			},
+			resolve(parent, args) {
+				return axios.delete(`http://localhost:3000/customers/${args.id}`).then((res) => res.data);
+			}
+		},
+		editCustomer: {
+			type: CustomerType,
+			args: {
+				id: {
+					type: new GraphQLNonNull(GraphQLString)
+				},
+				name: {
+					type: GraphQLString
+				},
+				email: {
+					type: GraphQLString
+				},
+				age: {
+					type: GraphQLInt
+				}
+			},
+			resolve(parent, args) {
+				return axios.patch(`http://localhost:3000/customers/${args.id}`, args).then((res) => res.data);
+			}
 		}
 	}
 });
